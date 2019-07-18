75a893fb1569e2c64bd5254fe84680aa8187065e
mzakharow <098098789>
2019-07-19 00:54:08 +0300

    weight table

--- booking/views.py
+++ booking/views.py
@@ -13,14 +13,27 @@ def index(request):
     first_date = datetime.datetime.now()
     weekday = datetime.datetime.now().weekday()
     weekdays = {0: 'ПН', 1: 'ВТ', 2: 'СР', 3: 'ЧТ', 4: 'ПТ', 5: 'СБ', 6: 'ВС'}
-    book_list = []
+    first_week = []
     for i in range(0, 7):
         date = first_date + datetime.timedelta(days=i - weekday)
         title = f'{weekdays.get(i)} {date.strftime("%d.%m.%Y")}'
         book_object, status = Booking.objects.get_or_create(day=date, defaults={'title': title})
-        book_list.append(book_object)
+        first_week.append(book_object)
+    week_list = [first_week]
 
-    context = {'booking': book_list}
+    # цикл сколько недель нужно после основной
+    for count in range(0, 3):
+        book_list = []
+        for key in weekdays:
+            date += datetime.timedelta(days=1)
+            title = f'{weekdays.get(key)} {date.strftime("%d.%m.%Y")}'
+            book_object, status = Booking.objects.get_or_create(day=date, defaults={'title': title})
+            book_list.append(book_object)
+
+        week_list.append(book_list)
+    # конец цикла
+
+    context = {'booking_main': first_week, 'booking_second': book_list, 'weeks': week_list}
     return HttpResponse(template.render(context, request))
 
 
