dcec80f6193774d8685db192360ca2f2ce297367
mzakharow <098098789>
2019-07-22 10:22:10 +0300

    heroku

--- book_workplace/settings.py
+++ book_workplace/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['bookingplaces.heroku.com']
+ALLOWED_HOSTS = ['bookingplaces.herokuapp.com']
 
 
 # Application definition
